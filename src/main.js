0e3ee1fd673df42b93d1a093a1080a6f2c0ae3a1
Igor <torpos86@mail.ru>
2020-12-13 19:51:41 +0300

    add mocks filters and show more button

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import { createUserRank } from "./view/user-rank.js";
 import { createFilmCounterTemplate } from "./view/filmCounter.js";
 import { createFullFilmDescription } from "./view/film-description.js";
 import { generateFilmCard } from "./mock/film.js";
+import { generateFilter } from "./mock/filter.js";
 
 const FILMS_COUNT = 15;
+const FILMS_COUNT_PER_STEP = 5;
 const FILMS_EXTRA_COUNT = 2;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilmCard);
+const filter = generateFilter(films);
 
 /**
    * Функция render выполняет отрисовку компонента в указанном месте HTML документа
@@ -28,20 +31,39 @@ render(siteHeaderElement, createUserRank(), 'beforeend');
 
 const siteMainElement = document.querySelector('.main');
 
-render(siteMainElement, createMenuTemplate(), 'beforeend');
+render(siteMainElement, createMenuTemplate(filter), 'beforeend');
 render(siteMainElement, createSortFilterTemplate(), 'beforeend');
 
 const siteFilmsList = siteMainElement.querySelector('.films');
 const siteFilmCard = siteFilmsList.querySelector('.films-list__container');
 const siteFooter = document.querySelector('.footer');
-for (let i = 0; i < FILMS_COUNT; i++) {
+for (let i = 0; i < Math.min(films.length, FILMS_COUNT_PER_STEP); i++) {
   render(siteFilmCard, createFilmCard(films[i]), 'beforeend');
 }
 
-render(siteFooter, createFullFilmDescription(films[0]), 'afterend');
-const siteShowMoreButton = siteFilmsList.querySelector('.films-list');
+//render(siteFooter, createFullFilmDescription(films[0]), 'afterend');
+const siteFilmsBoard = siteFilmsList.querySelector('.films-list');
+if (films.length > FILMS_COUNT_PER_STEP) {
+  let renderedFilmsCount = FILMS_COUNT_PER_STEP;
+  render(siteFilmsBoard, createLoadMoreButton(), 'beforeend');
+  const loadMoreButton = siteFilmsBoard.querySelector('.films-list__show-more');
 
-render(siteShowMoreButton, createLoadMoreButton(), 'beforeend');
+  siteFilmsBoard.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderedFilmsCount, renderedFilmsCount + FILMS_COUNT_PER_STEP)
+      .forEach((films) => render(siteFilmCard, createFilmCard(films), 'beforeend'));
+
+    renderedFilmsCount += FILMS_COUNT_PER_STEP;
+    if (renderedFilmsCount >= films.length) {
+      loadMoreButton.remove()
+    }
+  });
+}
+
+
+
+
 
 const siteFilmsListExtra = siteFilmsList.querySelectorAll('.films-list--extra');
 
