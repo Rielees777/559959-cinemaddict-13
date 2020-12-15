ac4a03576f2432b27c661da6c534b37ba3844cee
Igor <torpos86@mail.ru>
2020-12-15 19:43:45 +0300

    render function refact

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { createFilmCounterTemplate } from "./view/filmCounter.js";
 import { createFullFilmDescription } from "./view/film-description.js";
 import { generateFilmCard } from "./mock/film.js";
 import { generateFilter } from "./mock/filter.js";
+import { renderTemplate } from "./utils.js";
 
 const FILMS_COUNT = 15;
 const FILMS_COUNT_PER_STEP = 5;
@@ -21,27 +22,25 @@ const filter = generateFilter(films);
    * @param {HTMLElement} template сам компонент для добавления в документ
    * @param {string} place строка определяющая позицию добавляемого компонента по отношению к блоку HTML
    */
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-}
+
 
 const siteHeaderElement = document.querySelector('.header');
 
-render(siteHeaderElement, createUserRank(), 'beforeend');
+renderTemplate(siteHeaderElement, createUserRank(), 'beforeend');
 
 const siteMainElement = document.querySelector('.main');
 
-render(siteMainElement, createMenuTemplate(filter), 'beforeend');
-render(siteMainElement, createSortFilterTemplate(), 'beforeend');
+renderTemplate(siteMainElement, createMenuTemplate(filter), 'beforeend');
+renderTemplate(siteMainElement, createSortFilterTemplate(), 'beforeend');
 
 const siteFilmsList = siteMainElement.querySelector('.films');
 const siteFilmCard = siteFilmsList.querySelector('.films-list__container');
 const siteFooter = document.querySelector('.footer');
 for (let i = 0; i < Math.min(films.length, FILMS_COUNT_PER_STEP); i++) {
-  render(siteFilmCard, createFilmCard(films[i]), 'beforeend');
+  renderTemplate(siteFilmCard, createFilmCard(films[i]), 'beforeend');
 }
 
-//render(siteFooter, createFullFilmDescription(films[0]), 'afterend');
+renderTemplate(siteFooter, createFullFilmDescription(films[0]), 'afterend');
 const siteFilmsBoard = siteFilmsList.querySelector('.films-list');
 if (films.length > FILMS_COUNT_PER_STEP) {
   let renderedFilmsCount = FILMS_COUNT_PER_STEP;
@@ -61,21 +60,17 @@ if (films.length > FILMS_COUNT_PER_STEP) {
   });
 }
 
-
-
-
-
 const siteFilmsListExtra = siteFilmsList.querySelectorAll('.films-list--extra');
 
 siteFilmsListExtra.forEach((section) => {
   const siteFilmExtraCard = section.querySelector('.films-list__container');
   for (let i = 0; i < FILMS_EXTRA_COUNT; i++) {
-    render(siteFilmExtraCard, createFilmCard(films[i]), 'beforeend');
+    renderTemplate(siteFilmExtraCard, createFilmCard(films[i]), 'beforeend');
   }
 })
 
 
 const siteFilmCounter = siteFooter.querySelector('.footer__statistics');
 
-render(siteFilmCounter, createFilmCounterTemplate(), 'beforeend');
+renderTemplate(siteFilmCounter, createFilmCounterTemplate(), 'beforeend');
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+};
+
 // Функция генерирует случайное число в указанном диапазоне
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -11,4 +16,19 @@ export const getRandomElement = (items) => {
   const randomIndex = getRandomInteger(0, items.length - 1);
 
   return items[randomIndex];
-}
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
